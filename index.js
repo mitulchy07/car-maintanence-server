648d42d7ea63d35a2b9ff8544ebcd845810fdfed
mitulchy07 <mitul9616@gmail.com>
2022-11-06 14:35:10 +0100

    delete and update added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 4000;
@@ -8,20 +8,83 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER);
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}.mhsbjhf.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mhsbjhf.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db('test').collection('devices');
-  // perform actions on the collection object
-  console.log('Database connected with mongoDB');
-  client.close();
+
+async function run() {
+  try {
+    const serviceCollection = client
+      .db('geniusCarCollection')
+      .collection('services');
+
+    const orderCollection = client
+      .db('geniusCarCollection')
+      .collection('orders');
+
+    app.get('/services', async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+      console.log(query);
+      console.log(service);
+    });
+
+    //orders api
+    app.get('/orders', async (req, res) => {
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+
+    app.post('/orders', async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+
+    app.patch('/orders/:id', async (req, res) => {
+      const id = req.params.id;
+
+      const status = req.body.status;
+      const query = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: status,
+        },
+      };
+      const result = await orderCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
+
+    app.delete('/orders/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch((err) => {
+  console.error(err);
 });
 
 app.get('/', (req, res) => {
@@ -31,6 +94,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Genious Car server running on: ${port} `);
 });
-
-//user: dbuser1
-//password: crbZ9NxrEVeK6XCX
